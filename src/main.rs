44d48d3d9c3fc4af70897cd8237b099979ca9be4
parromd <me@davidparrott.dev>
2023-03-21 13:36:52 -0600

    key read file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-//use std::fs::File;
-//use std::future::Future;
-//use std::io::prelude::*;
-//use std::path::Path;
-//use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 use hyper::{ Client, Uri };
 use hyper_tls::HttpsConnector;
@@ -13,24 +11,21 @@ use tokio::io::{stdout, AsyncWriteExt as _};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-
+    let uri = "/data/2.5/weather?lat=40.24&lon=-111.65&units=imperial&appid=__KEY__";
     // TODO: get key and lat/lon from config file
     // (maybe all api params?)
-
-    //println!("{}", env::current_dir().unwrap().to_str().unwrap_or("none"));
-
-   /* 
+    
     let path = Path::new("./key");
     let mut file = match File::open(&path) {
         Ok(file) => file,
         Err(err) => panic!("unable to open file: {err}"),
     };
 
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Ok(_) => println!("{s}"),
-        Err(err) => panic!("unable to read file: {err}"),
-    }*/
+    let mut key = String::new();
+    match file.read_to_string(&mut key) {
+        Ok(_) => (),
+        Err(err) => panic!("unable to read key file: {err}"),
+    }
 
     let https = HttpsConnector::new();
     let client = Client::builder()
@@ -38,17 +33,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let uri = Uri::builder()
         .scheme("https")
         .authority("api.openweathermap.org")
+        .path_and_query(uri.replace("__KEY__", key.trim_end()))
         .build()
         .expect("failed to parse");
     let mut resp = client.get(uri).await?;
     println!("Response: {}", resp.status());
 
-
     while let Some(chunk) = resp.body_mut().data().await {
         stdout().write_all(&chunk?).await?;
     }
-
-
 
    Ok(()) 
 }
